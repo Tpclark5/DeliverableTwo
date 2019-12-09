2ca2d30874a4f3b32446b7dbf9b178af15598f23
Tim <TimothyClark@nexsystech.com>
2019-12-09 00:46:44 -0500

    Commit for Deliverable 2 of Lab 1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int checksum = 0;
+            string input = "";
+            string message = "";
+            Console.Write("Enter your message: ");
+            input = Console.ReadLine();
+            string inputUpper = input.ToUpper();
+
+            {
+                foreach (char item in inputUpper)
+                {
+                    string userCode = (item - 64).ToString();
+                    message += userCode + "-";
+                    checksum += item;
+
+                };
+
+                Console.WriteLine($"Your encoded message is {message}");
+                Console.WriteLine($"The checksum is {checksum}");
+            }
+        }
+    }
+}
